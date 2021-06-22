e134a3e576f05c03ff4ad86f76bc3a5d850478b1
ArcaneDisgea <magicdisgea@gmail.com>
2021-06-22 15:20:13 -0400

    update dice command to use dice notation

--- commands/fun/roll.js
+++ commands/fun/roll.js
@@ -1,36 +1,14 @@
+const { roll } = require("@airjp73/dice-notation");
 module.exports = {
   name: "roll",
   description: "Roll some up to a 20 sided die, 10 times or fewer.",
-  usage: "##roll [number of sides] [number of times]",
+  usage: "##roll 2d6 + 3",
   cooldown: 2,
   execute(message, args) {
     message.channel.startTyping();
-    function rollDice(max) {
-      return 1 + Math.floor(Math.random() * max);
-    }
-
-    let counter = 1;
-    let results = [];
-    let rolls = parseInt(args[1]);
-    let die = parseInt(args[0]);
-
-    function quantityRoll() {
-      if (counter <= rolls) {
-        counter++;
-        const dice = rollDice(die);
-        results.push(dice);
-        setTimeout(quantityRoll, 1000);
-        if (counter > rolls) {
-          message.channel.stopTyping();
-          return message.channel.send(results.toString().replace(/,/g, ", "));
-        }
-      }
-    }
-
-    if (rolls <= 10 && die <= 20) {
-      return quantityRoll();
-    }
+    const dice = args.toString().replace(/,/g, " ");
+    const result = roll(dice);
     message.channel.stopTyping();
-    return message.channel.send(":anger: You are using too many dice or asking for too many rolls. Please try again with less dice or fewer rolls.");
+    return message.channel.send(result.result)
   },
 };
